21cf590fa70faaa31b57e5ef11b1f053a1423a8f
Leonardo Di Giorgio <leonardodigiorgio@proton.me>
2026-03-16 22:16:07 -0400

    chore: call to win32_alloc_debug_console for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
+#include <cassert>
 
 #if defined WIN32
 
 #include <windows.h>
+#include "../include/win32_platform_layer.h"
+
 /* Windows entry point (WinMain) */
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		    PWSTR pCmdLine, int nCmdShow)
 {
+  /* DEBUGGING CONSOLE */
+  win32_alloc_debug_console();
+  
+  win32_create_window();
+
+
   
   return 0;
 }
