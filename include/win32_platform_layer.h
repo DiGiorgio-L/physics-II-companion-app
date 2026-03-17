d84c99115b1e1e9755691a0111fd6e9f34a147da
Leonardo Di Giorgio <leonardodigiorgio@proton.me>
2026-03-16 22:15:02 -0400

    feat: create function to allocate a debugging terminal

--- include/win32_platform_layer.h
+++ include/win32_platform_layer.h
@@ -1,6 +1,10 @@
 #ifndef WIN32_PLATFORM_LAYER_H
 #define WIN32_PLATFORM_LAYER_H
 
+#include <windows.h>
+#include <cstdint>
 
+void win32_alloc_debug_console();
+void win32_create_window();
 
 #endif
--- src/win32_platform_layer.cpp
+++ src/win32_platform_layer.cpp
@@ -1,3 +1,37 @@
 #include "../include/win32_platform_layer.h"
+#include <iostream>
+#include <cassert>
+#include <cstdio>
 
+#include <windows.h>
+
+void win32_alloc_debug_console()
+{
+  /* Allocate new console */
+  assert((AllocConsole() != 0) && "Could not allocate debugging terminal!");
+
+  /* Reopen stream buffers */
+  std::FILE *fp;
+  assert((freopen_s(&fp, "CONOUT$", "w", stdout) == 0)
+	 && "Could not reopen stdout stream buffer!");
+
+  assert((freopen_s(&fp, "CONOUT$", "w", stderr) == 0)
+	 && "Could not reopen stderr stream buffer!");
+
+  assert((freopen_s(&fp, "CONIN$", "r", stdin) == 0)
+	 && "Could not reopen stdin stream buffer!");
+
+  std::string title = "Debugging console";
+  assert((SetConsoleTitle(title.c_str()) != 0) && "Could not set console title!");
+  
+  return;
+}
+
+void win32_create_window()
+{
+  //win_handle = CreateWindowW();
+  //HWND handle = 0;
+  
+  return;
+}
 
