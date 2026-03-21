c0aef6c4f44d3d797388eb0b1b11777602d10483
Leonardo Di Giorgio <leonardodigiorgio@proton.me>
2026-03-21 17:58:37 -0400

    chore: make modifications to allow testing of sqrt() function

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,12 +5,13 @@
 int main()
 {
   std::cout << "MATH LIBRARY TEST MODULE!";
+  static int option = 0;
   do
   {
-    static int option = 0;
     std::cout << std::endl << "Choose the function to test: " << std::endl;
-    std::cout << "1. m_abs(x): " << std::endl;
-    std::cout << "2. m_sqr(x): " << std::endl;
+    std::cout << "1. m_abs(x): absolute value of a number" << std::endl;
+    std::cout << "2. m_sqr(x): square of a number" << std::endl;
+    std::cout << "3. m_sqrt(x): square root of a number " << std::endl;
     std::cout << "> ";
     std::cin >> option;
 
@@ -33,7 +34,17 @@ int main()
       std::cout << "Normal: " << number << std::endl;
       std::cout << "Fixed: " << std::fixed << number << std::endl;
       std::cout << "Scientific: " << std::scientific << number << std::endl;
-    }    
+    }
+        else if (option == 3)
+    {
+      double number = 0.0;
+      std::cout << "Introduce a number: ";
+      std::cin >> number;
+      number = m_sqrt(number);
+      std::cout << "Normal: " << number << std::endl;
+      std::cout << "Fixed: " << std::fixed << number << std::endl;
+      std::cout << "Scientific: " << std::scientific << number << std::endl;
+    }  
     
   } while (option != 0);
   std::cout << "TERMINATING PROGRAM!" << std::endl;
