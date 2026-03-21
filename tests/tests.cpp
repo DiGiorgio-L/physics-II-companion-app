f332976a789e467f4d92175e7c61d663af4546d7
Leonardo Di Giorgio <leonardodigiorgio@proton.me>
2026-03-21 17:39:49 -0400

    chore: improve math testing module

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,20 +4,39 @@
 
 int main()
 {
-  std::cout << "Choose the function to test: " << std::endl;
-  std::cout << "m_abs(): " << std::endl << "> ";
-  
-  int option = 0;
-  std::cin >> option;
-  if (option == 1)
+  std::cout << "MATH LIBRARY TEST MODULE!";
+  do
   {
-    double number = 0.0;
-    std::cout << "Introduce a number: ";
-    std::cin >> number;
-    number = m_abs(number);
-    std::cout << "Fixed: " << std::fixed << number << std::endl;
-    std::cout << "Scientific: " << std::scientific << number << std::endl;
-  }
+    static int option = 0;
+    std::cout << std::endl << "Choose the function to test: " << std::endl;
+    std::cout << "1. m_abs(x): " << std::endl;
+    std::cout << "2. m_sqr(x): " << std::endl;
+    std::cout << "> ";
+    std::cin >> option;
+
+    if (option == 1)
+    {
+      double number = 0.0;
+      std::cout << "Introduce a number: ";
+      std::cin >> number;
+      number = m_abs(number);
+      std::cout << "Normal: " << number << std::endl;
+      std::cout << "Fixed: " << std::fixed << number << std::endl;
+      std::cout << "Scientific: " << std::scientific << number << std::endl;
+    }
+    else if (option == 2)
+    {
+      double number = 0.0;
+      std::cout << "Introduce a number: ";
+      std::cin >> number;
+      number = m_sqr(number);
+      std::cout << "Normal: " << number << std::endl;
+      std::cout << "Fixed: " << std::fixed << number << std::endl;
+      std::cout << "Scientific: " << std::scientific << number << std::endl;
+    }    
+    
+  } while (option != 0);
+  std::cout << "TERMINATING PROGRAM!" << std::endl;
   
   return 0;
 }
