376b3d24edbc2c5b9e1c19ab93bf4cd65c3b0db4
Leonardo Di Giorgio <leonardodigiorgio@proton.me>
2026-03-21 17:05:24 -0400

    API: create absolute value function

---
+++ include/math.h
@@ -0,0 +1,9 @@
+#ifndef MATH_H
+#define MATH_H
+
+#include <cstdio>
+#include <cassert>
+
+double m_abs(double x);
+
+#endif
---
+++ src/utils/math.cpp
@@ -0,0 +1,7 @@
+#include "../../include/math.h"
+
+double m_abs(double x)
+{
+  if (x < 0) return -x;
+  else return x;
+}
